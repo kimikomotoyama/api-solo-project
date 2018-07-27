c4f88073af99da2b086345fd69c13372166ddda5
Kimiko Motoyama <meowlingual@gmail.com>
2018-07-27 23:53:04 +0900

    basic CRUD files

--- route/recipes/index.js
+++ route/recipes/index.js
@@ -3,7 +3,19 @@ const app = express();
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('hey there dude!');
-})
+  res.send('list all recipes');
+});
+
+router.post('/', (req, res) => {
+  res.send('post ok');
+});
+
+router.patch('/', (req, res) => {
+  res.send('patch ok');
+});
+
+router.delete('/', (req, res) => {
+  res.delete('delete ok');
+});
 
 module.exports = router;
