2615132c418682c0c781da2c3a66e75717838627
Kimiko Motoyama <meowlingual@gmail.com>
2018-07-28 00:03:04 +0900

    deleted unnecessary commented out codes

--- route/index.js
+++ route/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-// router.get('/', function (req, res) {
-//   res.send('hey there dude!');
-// })
-
 //declare require files
 const recipes = require("./recipes/index");
 
