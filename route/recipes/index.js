5e6df373c4a8a3e208468a0c2951cb289cadd401
Kimiko Motoyama <meowlingual@gmail.com>
2018-07-28 18:50:18 +0900

    req.body receives fine, after getting rid of router. Don't know why this works.

--- route/recipes/index.js
+++ route/recipes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
+// const app = express.app();
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer();
 // const knex = require("../../config.js");
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //requiring knex doesn't work, so I have to hardcode this for now. 
 var knex = require('knex')({
@@ -16,11 +22,11 @@ var knex = require('knex')({
 
 const root = '.'
 
-  router.get('/', function (req, res) {
+  app.get('/', function (req, res) {
     res.sendFile('/view/recipes/index.html', {root: root});
   });
   
-  router.get('/list', (req, res) => {
+  app.get('/list', (req, res) => {
     knex.select()
     .from("recipes")
     .then((data) => {
@@ -31,16 +37,22 @@ const root = '.'
     });
   });
   
-  router.post('/', (req, res) => {
-    res.send("post ok");
-  })
+  app.post('/',  upload.array(), function (req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+  });
+
+  // app.post('/', (req, res) => {
+  //   console.log(req.body);
+  //   res.send("post ok");
+  // })
   
-  router.patch('/', (req, res) => {
+  app.patch('/', (req, res) => {
     res.send('patch ok');
   });
   
-  router.delete('/', (req, res) => {
+  app.delete('/', (req, res) => {
     res.delete('delete ok');
   });
 
-module.exports = router;
+module.exports = app;
